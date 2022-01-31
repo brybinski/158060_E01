1cd84bed5f79467a249791101ec37eddeff9c5ac
Bartosz Rybiński <bartoszrybinski@outlook.com>
2022-01-31 12:50:00 +0100

    I have fun

--- 158060-E01/Z3/Z3.java
+++ 158060-E01/Z3/Z3.java
@@ -8,6 +8,9 @@ public class Z3 {
         String[] result = {};
         try{
             File f = new File(path);
+
+            // w javie 8 rozszerzenie było zawsze z dużych i wtedy robiłem .toUpperCase()
+            // to mniejsza szansa była na błąd użytkownika, ale 17 tak nie ma albo jest z małej to już nie kombinowałem
             result = f.list((dir, name) -> name.endsWith(extension));
         }
         catch (Exception e){
